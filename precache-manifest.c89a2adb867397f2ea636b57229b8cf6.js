8000efde8d693b93e1baf9a05b95238ba3186546
oalberto96 <ochoapublic@gmail.com>
2019-06-21 21:33:26 -0700

    Updates

---
+++ precache-manifest.c89a2adb867397f2ea636b57229b8cf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27f4881f6c2e0f591beeda54c492568f",
+    "url": "/laurini-gift/static/media/kitty.27f4881f.gif"
+  },
+  {
+    "revision": "d6011372b358638508669cd1fb53f573",
+    "url": "/laurini-gift/static/media/keanu.d6011372.gif"
+  },
+  {
+    "revision": "43bc70c2f616bfa96cb5",
+    "url": "/laurini-gift/static/js/runtime~main.2b088807.js"
+  },
+  {
+    "revision": "e926af6e358a0dd6d650",
+    "url": "/laurini-gift/static/js/main.5af9fe74.chunk.js"
+  },
+  {
+    "revision": "02a3c6fedd2c0ff2ddc6",
+    "url": "/laurini-gift/static/js/2.40cd7849.chunk.js"
+  },
+  {
+    "revision": "e926af6e358a0dd6d650",
+    "url": "/laurini-gift/static/css/main.82e4f922.chunk.css"
+  },
+  {
+    "revision": "76f1da1acfe8390b8532a44d86b33a13",
+    "url": "/laurini-gift/index.html"
+  }
+];
